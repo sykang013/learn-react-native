2184f619f9e524bb20a3d3a096af284d17271cd4
sykang013 <sy.kang013@gmail.com>
2023-06-07 19:56:10 +0900

    style. 4일간의 날씨 정보 페이지네이션 포함 스타일 완료

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from "expo-status-bar"; // third-party package
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Dimensions, View, ScrollView } from "react-native";
+// ES6문법 객체 안의 width를 가져와서 이름을 SCREEN_WIDTH로 바꾼다
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+// const SCREEN_WIDTH = Dimensions.get("window");
 
+console.log(SCREEN_WIDTH);
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
@@ -25,7 +34,7 @@ export default function App() {
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
@@ -45,12 +54,9 @@ const styles = StyleSheet.create({
     fontSize: 58,
     fontWeight: 500,
   },
-  weather: {
-    flex: 3,
-  },
+  weather: {},
   day: {
-    flex: 1,
-    backgroundColor: "teal",
+    width: SCREEN_WIDTH,
     alignItems: "center",
   },
   temp: {
