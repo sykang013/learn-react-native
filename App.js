e573ebb6559b37f2d872f04280a9dc96f5038fb1
sykang013 <sy.kang013@gmail.com>
2023-06-07 21:25:59 +0900

    feat. 날씨 API 추가

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
+import * as Location from "expo-location";
 import { StatusBar } from "expo-status-bar"; // third-party package
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, Dimensions, View, ScrollView } from "react-native";
 // ES6문법 객체 안의 width를 가져와서 이름을 SCREEN_WIDTH로 바꾼다
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 // const SCREEN_WIDTH = Dimensions.get("window");
 
-console.log(SCREEN_WIDTH);
+const API_KEY = "a1da58522d99fbca59a77e361689bc8d";
+
+// console.log(SCREEN_WIDTH);
 export default function App() {
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    // const location = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`
+    );
+    const json = await response.json();
+    console.log(json);
+  };
+  //컴포넌트가 마운트 되면 useEffect를 사용해서 getPermissions function 호출
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
