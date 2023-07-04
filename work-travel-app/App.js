3c09cc6f23735d66e4650f5800ed9c9134b28977
sykang013 <sy.kang013@gmail.com>
2023-07-04 09:25:16 +0900

    feat: work, travel  클릭 기능 추가

--- work-travel-app/App.js
+++ work-travel-app/App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity, //opacity 애니메이션 있음
+  TouchableHighlight, //배경색 하이라이트
+  TouchableWithoutFeedback, //아무 반응도 일어나지 않음
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity activeOpacity={0}>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableHighlight
+          underlayColor="red"
+          onPress={() => console.log("pressed")}
+        >
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 }
@@ -13,8 +32,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingVertical: 20, //CSS에서는 없는 속성
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 40,
+    fontWeight: 600,
+    color: theme.grey,
   },
 });
