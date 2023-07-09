ca72fd31726ba84b9069d2fb433082534a7b0d8d
sykang013 <sy.kang013@gmail.com>
2023-07-09 23:17:20 +0900

    feat: 스크롤 뷰 구현, input 입력값 표시 및 스타일링

--- work-travel-app/App.js
+++ work-travel-app/App.js
@@ -8,6 +8,7 @@ import {
   TouchableHighlight, //배경색 하이라이트
   TouchableWithoutFeedback, //아무 반응도 일어나지 않음
   TextInput,
+  ScrollView,
 } from "react-native";
 import { theme } from "./colors";
 
@@ -17,9 +18,9 @@ export default function App() {
   const [working, setWorking] = useState(true);
   //user가 입력한 값 저장
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   //travel 함수
   const travel = () => setWorking(false);
-  const [toDos, setToDos] = useState();
   //work 함수
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
@@ -29,19 +30,26 @@ export default function App() {
       //todo의 현재값(text)이 비어있다면 함수 종료
       return;
     }
-    const newToDos = Object.assign(
-      //Object.assign으로 두 가지 객체 합쳐서 내보내기
-      {}, //target object
-      toDos, //이전의 toDos
-      {
-        [Date.now()]: { text, work: working }, //새로운 todo
-      }
-    );
+    //Object.assign 활용하여 합쳐진 객체 생성하기
+    // const newToDos = Object.assign(
+    //   //Object.assign으로 두 가지 객체 합쳐서 내보내기
+    //   {}, //target object
+    //   toDos, //이전의 toDos
+    //   {
+    //     //계산된 속성명(computed property name)으로 Date.now()가 반환하는 값을 지정해주고 있음.
+    //     //객체 리터럴에서 키를 [] 괄호로 감쌀 경우, 해당 표현식이 계산된 값을 속성 이름으로 사용한다.
+    //     [Date.now()]: { text, work: working }, //새로운 todo
+    //   }
+    // );
     // save to do 나중에 할 것(일단은 공란으로 둔다)
+    //다른방법
+    const newToDos = {
+      ...toDos, //toDos의 값들을 전개한 값
+      [Date.now()]: { text, work: working }, //계산된 속성명을 통한 Date.now()함수 반환값을 아이디로,
+    };
     setToDos(newToDos);
     setText("");
   };
-  console.log(toDos);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -70,16 +78,27 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <TextInput
-        // multiline
-        // secureTextEntry
-        // keyboardType="numbers-and-punctuation"
-        // returnKeyType="yahoo"
         onSubmitEditing={addToDo}
-        value={text}
         onChangeText={onChangeText}
+        returnKeyType="done"
+        value={text}
         placeholder={working ? "Add a To Do" : "Where do you wanna go?"}
         style={styles.input}
       />
+      {/* 스크롤이 가능한 컨테이너 역할 */}
+      <ScrollView>
+        {/* map을 돌리기 위해 Object의 키들을 배열로 추출keys() 한 후 map 돌림 */}
+        {Object.keys(toDos).map(
+          (
+            key //toDos객체 안의 key(id)들을 배열로 반환, map메서드로 각 키를 순회하며 View,Text 생성
+          ) => (
+            <View style={styles.toDo} key={key}>
+              {/* toDos */}
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            </View>
+          )
+        )}
+      </ScrollView>
     </View>
   );
 }
@@ -105,7 +124,19 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 16,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
